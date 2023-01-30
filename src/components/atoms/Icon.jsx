8a8536afed04d7c33a5521b4a93cd1c636ff37b0
Kevin Mamaqi Kapllani <108928180+kevinmamaqi-90poe@users.noreply.github.com>
2023-01-30 04:55:00 -0300

    primera-revision

--- src/components/atoms/Icon.jsx
+++ src/components/atoms/Icon.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 const Span = styled.span`
-  font-size: x-large;
+  font-size: x-large; // Utiliza un tamaño de fuente que venga de styles
 `
 export default function Icon({ children }) {
   return <Span className="material-symbols-outlined">{children}</Span>
